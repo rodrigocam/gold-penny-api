4d65312916e4e5856fa6aaa5611e8efb7f7ff08b
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-16 23:59:58 -0300

    Create some templates

--- play_vendas/frontend/views.py
+++ play_vendas/frontend/views.py
@@ -3,8 +3,12 @@ from django.contrib.auth import authenticate
 
 def index(request):
     user = authenticate(request.user)
+    event = 'Vila Mix'
+    total_earnings = 250.00
+    products = ['Cerveja', 'Vodka', 'Whisky']
+    ctx = {'event': event, 'total_earnings': total_earnings, 'products': products}
 
-    if user:
-        return render(request, 'frontend/home.jinja2')
+    #if user:
+    return render(request, 'frontend/home.jinja2', ctx)
 
-    return render(request, 'frontend/login.jinja2')
+    #return render(request, 'frontend/login.jinja2')
