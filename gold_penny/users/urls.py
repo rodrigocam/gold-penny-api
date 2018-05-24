80e767e4e7d993db5e3da25f804e86b1f9f4b3db
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-23 23:19:02 -0300

    CD testing commit

--- gold_penny/users/urls.py
+++ gold_penny/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('login/', auth_views.login, {'template_name': 'users/login.jinja2'}, name='login'),
     path('logout/', auth_views.logout, {'next_page': 'index'}, name='logout')
+    path('testing/', index, name='testing')
 ]
