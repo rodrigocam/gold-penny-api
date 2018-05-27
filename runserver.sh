1ad7ebb88b554eb67c5e4007b7bddc6bf72a1273
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-27 20:34:39 -0300

    Add flag no input to manage.py collectstatic

--- runserver.sh
+++ runserver.sh
@@ -14,6 +14,6 @@ else
 	sleep 5
     python manage.py makemigrations
     python manage.py migrate
-    python manage.py collectstatic
+    python manage.py collectstatic --no-input
     python manage.py runserver 0.0.0.0:80 --insecure
 fi
