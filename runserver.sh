6067beced295450f0c9b9b66572219a7baef831f
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-27 20:10:03 -0300

    Fix not serving static files

--- runserver.sh
+++ runserver.sh
@@ -14,5 +14,6 @@ else
 	sleep 5
     python manage.py makemigrations
     python manage.py migrate
-    python manage.py runserver 0.0.0.0:80
+    python manage.py collectstatic
+    python manage.py runserver 0.0.0.0:80 --insecure
 fi
