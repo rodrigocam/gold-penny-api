f92a4f6ebbfc3144de55090edb40ce84cecb8a45
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-20 23:36:42 -0300

    Debugging

--- goldpenny/events/utils.py
+++ goldpenny/events/utils.py
@@ -5,9 +5,11 @@ from .models import Product, Event
 def sell_product(user, product_id, amount):
     with transaction.atomic():
         event = Event.objects.filter(user=user)[0]
+        print(f'EVENT {event}')
         product = Product.objects.select_for_update().get(
             event=event,
             id=product_id
         )
         product.total_sold += amount
+        print(f'PRODUCT {product}')
         product.save()
